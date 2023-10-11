897f8075a5564fc512754460dcd18a7027390716
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2023-10-11 13:26:20 -0400

    typescript

--- main.ts
+++ main.ts
@@ -1,8 +1,41 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Julianne Leblanc-Peltier
+ * Created on: Sep 2023
+ * This program turns on and changes the colors of the RobotBit
 */
 
-basic.showString('Hello, World!')
+let neopixelStrip: neopixel.Strip = null
+
+// cleanup
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+  basic.showIcon(IconNames.Happy)
+  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+  basic.pause(1000)
+  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+  basic.pause(1000)
+  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+  basic.pause(1000)
+  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.show()
+})
+
+input.onButtonPressed(Button.B, function () {
+  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.show()
+  basic.showIcon(IconNames.Happy)
+})
