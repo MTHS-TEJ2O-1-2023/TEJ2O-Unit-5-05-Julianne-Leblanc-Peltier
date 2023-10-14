b4896ba9135bba0e2cd7325d4054cbbcc9ba845d
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2023-10-14 04:21:21 +0000

    stuff is done :P

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -18,21 +18,14 @@ display.show(Image.HAPPY)
 while True:
     if button_a.was_pressed():
         display.show(Image.HAPPY)
-        neopixelstrip[0] = (0, 255, 0)
+        neopixelstrip[2] = (0, 255, 0)
         sleep(300)
-        neopixelstrip[0] = (0, 0, 0)
+        neopixelstrip[2] = (0, 0, 0)
 
         neopixelstrip[1] = (255, 255, 0)
         sleep(200)
         neopixelstrip[1] = (0, 0, 0)
 
-        neopixelstrip[2] = (255, 0, 0)
+        neopixelstrip[0] = (255, 0, 0)
         sleep(100)
-        neopixelstrip[2] = (0, 0, 0)
-
-    if button_b.was_pressed():
-        display.show(Image.HAPPY)
         neopixelstrip[0] = (0, 0, 0)
-        neopixelstrip[1] = (0, 0, 0)
-        neopixelstrip[2] = (0, 0, 0)
-        neopixelstrip[3] = (0, 0, 0)
